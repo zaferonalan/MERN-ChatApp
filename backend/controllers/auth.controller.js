70fab1dbd9c64528c9c27c0504f6705625c6ce21
zafergunayonalan <zafergunay7@gmail.com>
2025-04-19 21:31:01 +0300

    logOut-created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -49,10 +49,37 @@ export const signup = async(req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    res.send("loginUser")
+export const login = async(req, res) => {
+    try {
+        const {userName, password} = req.body
+        const user = await User.findOne({userName})
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({error: "Invalid username or password"})
+        }
+
+        generateTokenAndSetCookie(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            userName: user.userName,
+            profilepic: user.profilepic
+        })
+
+    } catch (error) {
+        console.log("Erorr in login controller", error.message)
+        res.status(500).json({error: "Internal Server Error"})
+    }
 }
 
 export const logout = (req, res) => {
-    res.send("logoutUser")
+    try {
+        res.cookie("jwt", "", {maxAge:0})
+        res.status(200).json({message: "Logged out successfuly"})
+    } catch (error) {
+        console.log("Erorr in logout controller", error.message)
+        res.status(500).json({error: "Internal Server Error"})
+    }
 }
