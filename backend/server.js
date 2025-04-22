3f276ae7ffe22681925ca9111086698063503dae
zafergunayonalan <zafergunay7@gmail.com>
2025-04-22 22:48:28 +0300

    userController-created

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messagesRoute from "./routes/message.routes.js";
+import userRoute from "./routes/user.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express()
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message",messagesRoute)
+app.use("/api/users", userRoute)
 
 app.listen(5000, () => {
     connectToMongoDB()
