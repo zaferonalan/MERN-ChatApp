6682e2dd92727bd8bac0aa560e5441f19962ea48
zafergunayonalan <zafergunay7@gmail.com>
2025-04-22 21:24:28 +0300

    messageController-created

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -38,3 +38,28 @@ export const sendMessage = async(req,res) => {
         res.status(500).json({error:"Internal server error"})
     }
 }
+
+export const getMessages = async(req, res) => {
+    try {    
+
+        const {id: userToChatId} = req.params
+        const senderId = req.user._id
+
+        const conversation = await Conversation.findOne({
+            participants: {$all: [senderId, userToChatId]}
+        }).populate("messages")
+
+
+        if (!conversation) {
+           return res.status(200).json([])
+        }
+
+        const messages = conversation.messages
+
+        res.status(200).json(messages)
+
+    } catch (error) {
+        console.log("Error in getMessages:", error.message)
+        res.status(500).json({error: "Interna Server Error"})
+    }
+}
