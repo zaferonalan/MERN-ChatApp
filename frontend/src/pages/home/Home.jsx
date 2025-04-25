bce4ebfef8a1528c6145bd7d09adef123528930e
zafergunayonalan <zafergunay7@gmail.com>
2025-04-25 17:58:10 +0300

    Sidebar-created

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400/10 bg-clip-padding backdrop-blur-lg'>
+			<Sidebar />
+			{/* <MessageContainer /> */}
+		</div>
   )
 }
 
