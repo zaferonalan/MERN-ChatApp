9e39a57b03a99c7f41ab94c02268b9b1e4b0029a
zafergunayonalan <zafergunay7@gmail.com>
2025-04-25 23:36:34 +0300

    messages-Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Login from './pages/login/login'
-import SignUp from './pages/signup/SignUp'
 import Home from './pages/home/home'
 
 const App = () => {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <Login/> */}
-      {/* <SignUp/> */}
       <Home/>
     </div>
   )
---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,20 @@
+import Message from './Message'
+
+const messages = () => {
+  return (
+    <div className='px-4 flex-1 overflow-auto'>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+    </div>
+  )
+}
+
+export default messages
