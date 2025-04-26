cd2dba0322fe527d02fe6d7789395da6ccf98c82
zafergunayonalan <zafergunay7@gmail.com>
2025-04-26 16:55:25 +0300

    editMain&Port

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Home from './pages/home/home'
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+      </Routes>
     </div>
   )
 }
